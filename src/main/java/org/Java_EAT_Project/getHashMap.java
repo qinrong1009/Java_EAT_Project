edc0c2b6b01ce16ea49ad920014a89da889d7440
林郁庭 <linyuting@linyutingdeMacBook-Air.local>
2024-06-13 17:24:12 +0800

    getHashMap.java 01

--- src/main/java/org/Java_EAT_Project/getHashMap.java
+++ src/main/java/org/Java_EAT_Project/getHashMap.java
@@ -1,5 +1,32 @@
 package org.Java_EAT_Project;
+import java.util.*;
+import java.util.Map;
+
+//import java.lang.*;
 
 public class getHashMap {
-    
+    Map<Integer, Place> eatMap = new HashMap<>();
+     
+    public ArrayList<Place> getHashMap(Set<Integer> set){
+        //deserialize restaurant map
+        //Map<Integer, Place> eatMap = Serialization.deserializePlaces("places.ser");
+        //set tree map to get rating order
+        Map<Double, Place> treeMap =new TreeMap<>();
+        for (int i:set){
+            Place want = eatMap.get(i);
+            treeMap.put(want.getRating(), want);
+        }
+        ArrayList<Place> orderedArrayList = new ArrayList<>();
+        //put place in arraylist
+        for(Map.Entry<Double, Place> entry: treeMap.entrySet()){
+            orderedArrayList.add(entry.getValue());
+        }
+        return orderedArrayList;
+
+    }
+
+    public void readHashMap(String fileName){
+        this.eatMap = Serialization.deserializePlaces(fileName);
+    }
 }
+
