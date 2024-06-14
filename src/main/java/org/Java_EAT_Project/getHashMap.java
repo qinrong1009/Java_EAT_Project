a71aeef681fedc63492b1ddc662419eff830bc1b
林郁庭 <linyuting@linyutingdeMacBook-Air.local>
2024-06-14 21:05:24 +0800

    getHashMap 02

--- src/main/java/org/Java_EAT_Project/getHashMap.java
+++ src/main/java/org/Java_EAT_Project/getHashMap.java
@@ -11,15 +11,30 @@ public class getHashMap {
         //deserialize restaurant map
         //Map<Integer, Place> eatMap = Serialization.deserializePlaces("places.ser");
         //set tree map to get rating order
-        Map<Double, Place> treeMap =new TreeMap<>();
+
+        Map<Double, ArrayList<Place>> treeMap =new TreeMap<>();
         for (int i:set){
             Place want = eatMap.get(i);
-            treeMap.put(want.getRating(), want);
+            double wantRate= want.getRating();
+            if (treeMap.get(wantRate)==null){
+                ArrayList<Place> getArrayList = new ArrayList<>();
+                getArrayList.add(want);
+                treeMap.put(wantRate, getArrayList);
+            }else{
+                ArrayList<Place> getArrayList = treeMap.get(wantRate);
+                getArrayList.add(want);
+                treeMap.put(wantRate, getArrayList);
+            }
+            //treeMap.put(want.getRating(), want);
         }
         ArrayList<Place> orderedArrayList = new ArrayList<>();
         //put place in arraylist
-        for(Map.Entry<Double, Place> entry: treeMap.entrySet()){
-            orderedArrayList.add(entry.getValue());
+        for(Map.Entry<Double, ArrayList<Place>> entry: treeMap.entrySet()){
+            ArrayList<Place> rests=entry.getValue();
+            for(Place res: rests){
+                orderedArrayList.add(res);
+            }
+            //orderedArrayList.add(entry.getValue());
         }
         return orderedArrayList;
 
